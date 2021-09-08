b0573661672ddde233e53fa48472f1af11e552ac
Azat Salimov <legendkris@yandex.ru>
2021-09-08 23:28:56 +0500

    edit logic

--- routes/movies.js
+++ routes/movies.js
@@ -4,15 +4,20 @@ const {
   getMovies,
   deleteMovie,
   createMovie,
+  likeMovie,
+  dislikeMovie,
 } = require('../controllers/movies');
 
 const {
   createMovieValidation,
   deleteMovieValidation,
+  changeLikeValidation,
 } = require('../middlewares/celebrate');
 
 router.get('/', getMovies);
 router.post('/', createMovieValidation, createMovie);
+router.put('/:movieId/likes', changeLikeValidation, likeMovie);
+router.delete('/:movieId/likes', changeLikeValidation, dislikeMovie);
 router.delete('/:movieId', deleteMovieValidation, deleteMovie);
 
 module.exports = router;
